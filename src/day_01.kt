ebcf36dfc03542f84e48206bcbdffe3115f7391d
Dmitri Lakachauskis <d@dmitri.io>
2022-12-14 23:44:12 +0200

    Day 2 - puzzle 2.

--- src/day_01.kt
+++ src/day_01.kt
@@ -1,27 +1,28 @@
-fun main() {
-    fun reduceLines(lines: List<String>): List<Int> {
-        var index = 0
+private fun reduceLines(lines: List<String>): List<Int> {
+    var index = 0
 
-        return lines.fold(mutableListOf(0)) { list, line ->
-            if (line.isBlank()) {
-                list.add(++index, 0)
-            } else {
-                list[index] += line.toInt()
-            }
-
-            list
+    return lines.fold(mutableListOf(0)) { list, line ->
+        if (line.isBlank()) {
+            list.add(++index, 0)
+        } else {
+            list[index] += line.toInt()
         }
+
+        list
     }
+}
 
-    fun puzzle1(input: List<String>) = reduceLines(input).max()
+private fun puzzle1(input: List<String>) = reduceLines(input).max()
 
-    fun puzzle2(input: List<String>) = reduceLines(input)
-        .sortedDescending()
-        .take(3)
-        .sum()
+private fun puzzle2(input: List<String>) = reduceLines(input)
+    .sortedDescending()
+    .take(3)
+    .sum()
 
+fun main() {
     val testInput = readInput("day_01_input_test")
     check(puzzle1(testInput) == 24000)
+    check(puzzle2(testInput) == 45000)
 
     val input = readInput("day_01_input")
     puzzle1(input).println()
--- src/day_02.kt
+++ src/day_02.kt
@@ -1,10 +1,19 @@
-enum class Outcome(val points: Int) {
+private enum class Outcome(val points: Int) {
     WIN(6),
     DRAW(3),
-    LOSS(0)
+    LOSS(0);
+
+    companion object {
+        fun fromChar(char: Char) = when (char) {
+            'X' -> LOSS
+            'Y' -> DRAW
+            'Z' -> WIN
+            else -> error("Unsupported character.")
+        }
+    }
 }
 
-enum class Shape(val points: Int) {
+private enum class Shape(val points: Int) {
     ROCK(1),
     PAPER(2),
     SCISSORS(3);
@@ -14,33 +23,57 @@ enum class Shape(val points: Int) {
             'A', 'X' -> ROCK
             'B', 'Y' -> PAPER
             'C', 'Z' -> SCISSORS
-            else -> error("Unsupported character")
+            else -> error("Unsupported character.")
         }
     }
 }
 
-typealias Game = Pair<Shape, Shape>
+private typealias Game = Pair<Shape, Shape>
 
-val Game.outcome get() = when (this) {
-    Shape.ROCK to Shape.ROCK,
-    Shape.PAPER to Shape.PAPER,
-    Shape.SCISSORS to Shape.SCISSORS -> Outcome.DRAW
-    Shape.ROCK to Shape.SCISSORS,
-    Shape.PAPER to Shape.ROCK,
-    Shape.SCISSORS to Shape.PAPER -> Outcome.LOSS
-    else -> Outcome.WIN
-}
+private typealias Prediction = Pair<Shape, Outcome>
 
-val Game.myScore get() = second.points + outcome.points
+private val Game.outcome
+    get() = when (this) {
+        Shape.ROCK to Shape.ROCK,
+        Shape.PAPER to Shape.PAPER,
+        Shape.SCISSORS to Shape.SCISSORS -> Outcome.DRAW
+
+        Shape.ROCK to Shape.SCISSORS,
+        Shape.PAPER to Shape.ROCK,
+        Shape.SCISSORS to Shape.PAPER -> Outcome.LOSS
+
+        else -> Outcome.WIN
+    }
+
+private val Game.myScore get() = second.points + outcome.points
+
+private val Prediction.game
+    get() = when (this) {
+        Shape.ROCK to Outcome.WIN,
+        Shape.PAPER to Outcome.DRAW,
+        Shape.SCISSORS to Outcome.LOSS -> first to Shape.PAPER
+
+        Shape.PAPER to Outcome.LOSS,
+        Shape.ROCK to Outcome.DRAW,
+        Shape.SCISSORS to Outcome.WIN -> first to Shape.ROCK
+
+        else -> first to Shape.SCISSORS
+    }
+
+private fun createGame(moves: String) = Shape.fromChar(moves[0]) to Shape.fromChar(moves[2])
+
+private fun createPrediction(moves: String) = Shape.fromChar(moves[0]) to Outcome.fromChar(moves[2])
+
+private fun puzzle1(input: List<String>) = input.map(::createGame).sumOf { it.myScore }
+
+private fun puzzle2(input: List<String>) = input.map(::createPrediction).sumOf { it.game.myScore }
 
 fun main() {
-    fun createGame(moves: String) = Shape.fromChar(moves[0]) to Shape.fromChar(moves[2])
-
-    fun puzzle1(input: List<String>) = input.map(::createGame).sumOf { it.myScore }
-
     val testInput = readInput("day_02_input_test")
     check(puzzle1(testInput) == 15)
+    check(puzzle2(testInput) == 12)
 
     val input = readInput("day_02_input")
     puzzle1(input).println()
+    puzzle2(input).println()
 }
