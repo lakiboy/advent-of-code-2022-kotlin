c49ca1ba9f8c6605fcfcd159114b00f68066bc87
Dmitri Lakachauskis <d@dmitri.io>
2022-12-15 13:35:40 +0200

    Day 6 - puzzle 1 & 2.

---
+++ src/day_06.kt
@@ -0,0 +1,17 @@
+// In-effective, but single expression :)
+private fun puzzle(input: String, len: Int) = (0 until input.length - len)
+    .first { index -> input.substring(index, index + len).toSet().size == len } + len
+
+fun main() {
+    check(puzzle(readText("day_06_input_test_1"), 4) == 5)
+    check(puzzle(readText("day_06_input_test_2"), 4) == 6)
+    check(puzzle(readText("day_06_input_test_3"), 4) == 10)
+
+    check(puzzle(readText("day_06_input_test_1"), 14) == 23)
+    check(puzzle(readText("day_06_input_test_2"), 14) == 23)
+    check(puzzle(readText("day_06_input_test_3"), 14) == 29)
+
+    val input = readText("day_06_input")
+    puzzle(input, 4).println()
+    puzzle(input, 14).println()
+}
