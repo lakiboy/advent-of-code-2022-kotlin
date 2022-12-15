6ffcec048bd72bde9f3784412eeb28970bea3afe
Dmitri Lakachauskis <d@dmitri.io>
2022-12-15 21:08:03 +0200

    Day 8 - puzzle 1 & 2.

---
+++ src/day_08.kt
@@ -0,0 +1,109 @@
+// No optimization e.g. no cache, no tail recursion.
+
+typealias Trees = List<List<Int>>
+typealias TreeCoords = Pair<Int, Int>
+
+private fun TreeCoords.top() = Pair(first - 1, second)
+private fun TreeCoords.bottom() = Pair(first + 1, second)
+private fun TreeCoords.left() = Pair(first, second - 1)
+private fun TreeCoords.right() = Pair(first, second + 1)
+
+private class TreeGrid private constructor(private val trees: Trees) {
+    private val size = trees.size
+
+    private val alwaysVisible = size * 4 - 4
+
+    private val lastIndex = size - 1
+
+    private val range = 1 until lastIndex
+
+    private val fullRange = 0 until size
+
+    val visibleCount
+        get() = range.fold(alwaysVisible) { acc, row ->
+            acc + range.count { col -> Pair(row, col).isVisible }
+        }
+
+    val scenicScore
+        get() = fullRange.maxOf { row ->
+            fullRange.maxOf { col -> Pair(row, col).visibleTrees }
+        }
+
+    private val TreeCoords.isEdge get() = first == 0 || second == 0 || first == lastIndex || second == lastIndex
+
+    private operator fun TreeCoords.compareTo(other: TreeCoords) =
+        trees[first][second] - trees[other.first][other.second]
+
+    private val TreeCoords.isVisible: Boolean
+        get() = when {
+            exceedsDirection(this, this) { top() } -> true
+            exceedsDirection(this, this) { bottom() } -> true
+            exceedsDirection(this, this) { left() } -> true
+            exceedsDirection(this, this) { right() } -> true
+            else -> false
+        }
+
+    private val TreeCoords.visibleTrees
+        get(): Int {
+            var total = 1
+
+            if (first > 0) {
+                total *= visibleDirectionCount(this, this) { top() }
+            }
+            if (first < lastIndex) {
+                total *= visibleDirectionCount(this, this) { bottom() }
+            }
+            if (second > 0) {
+                total *= visibleDirectionCount(this, this) { left() }
+            }
+            if (second < lastIndex) {
+                total *= visibleDirectionCount(this, this) { right() }
+            }
+
+            return total
+        }
+
+    private fun exceedsDirection(target: TreeCoords, compare: TreeCoords, next: TreeCoords.() -> TreeCoords): Boolean {
+        val neighbour = compare.next()
+
+        return if (target > neighbour) {
+            return if (neighbour.isEdge) true else exceedsDirection(target, neighbour, next)
+        } else {
+            false
+        }
+    }
+
+    private fun visibleDirectionCount(target: TreeCoords, compare: TreeCoords, next: TreeCoords.() -> TreeCoords): Int {
+        val neighbour = compare.next()
+
+        if (target <= neighbour || neighbour.isEdge) {
+            return 1
+        }
+
+        return visibleDirectionCount(target, neighbour, next) + 1
+    }
+
+    companion object {
+        fun fromLines(lines: List<String>): TreeGrid {
+            val trees = lines.map { line ->
+                line.map { tree -> tree.digitToInt() }
+            }
+
+            return TreeGrid(trees)
+        }
+    }
+}
+
+private fun puzzle1(lines: List<String>) = TreeGrid.fromLines(lines).visibleCount
+
+private fun puzzle2(lines: List<String>) = TreeGrid.fromLines(lines).scenicScore
+
+fun main() {
+    val testInput = readInput("day_08_input_test")
+    check(puzzle1(testInput) == 21)
+    check(puzzle2(testInput) == 8)
+
+    val input = readInput("day_08_input")
+    puzzle1(input).println()
+    puzzle2(input).println()
+}
