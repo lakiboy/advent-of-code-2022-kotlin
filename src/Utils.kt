c28ccb7a054a2adb6ddaa71843623ac836806c01
Dmitri Lakachauskis <d@dmitri.io>
2022-12-15 13:35:40 +0200

    Day 6 - puzzle 1 & 2.

--- src/Utils.kt
+++ src/Utils.kt
@@ -1,6 +1,4 @@
 import java.io.File
-import java.math.BigInteger
-import java.security.MessageDigest
 
 fun readInput(name: String) = File("src", "$name.txt").readLines()
 
@@ -8,8 +6,3 @@ fun readText(name: String) = File("src", "$name.txt").readText()
 
 // Shorthand for printing output.
 fun Any?.println() = println(this)
-
-// Converts string to md5 hash.
-fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
-    .toString(16)
-    .padStart(32, '0')
