27c37351a6c17ed73a2f4fb88089ac2ce868c170
Dmitri Lakachauskis <d@dmitri.io>
2022-12-28 13:54:16 +0200

    Day 12 - puzzle 1 - change to BFS.

--- src/day_12/puzzle.kt
+++ src/day_12/puzzle.kt
@@ -3,7 +3,6 @@ package day_12
 import day_12.Direction.*
 import println
 import readInput
-import kotlin.math.absoluteValue
 
 enum class Direction { UP, DOWN, LEFT, RIGHT }
 
@@ -36,31 +35,31 @@ class HeightMap(lines: List<String>) {
     }
     private val visited = mutableSetOf<Point>()
 
-    var distance = 0
-        private set
-
-    init {
-        dfs(start, 0)
-    }
-
     private val Point.valid get() = x >= 0 && y >= 0 && y <= grid.lastIndex && x <= grid[y].lastIndex
 
     private val Point.neighbours get() = Direction
         .values()
         .map { move(it) }
-        .filter { to -> to.valid }
-        .filter { to -> to !in visited }
-        .filter { to -> grid[y][x] + 1 >= grid[to.y][to.x] }
-        .sortedBy { to -> (to.x - finish.x).absoluteValue + (to.y - finish.y).absoluteValue - grid[to.y][to.x] }
+        .filter { to -> to.valid && to !in visited && grid[y][x] + 1 >= grid[to.y][to.x] }
 
-    private fun dfs(node: Point, depth: Int) {
-        visited.add(node)
+    val distance get(): Int {
+        val queue = mutableListOf<Pair<Point, Int>>()
+        queue.add(start to 0)
 
-        if (node == finish) {
-            distance = depth
-        } else {
-            node.neighbours.forEach { neighbour -> dfs(neighbour, depth + 1) }
+        while (queue.isNotEmpty()) {
+            val (node, distance) = queue.removeFirst()
+
+            if (node == finish) {
+                return distance
+            }
+
+            node.neighbours.forEach { neighbour ->
+                queue.add(neighbour to (distance + 1))
+                visited.add(neighbour)
+            }
         }
+
+        return -1
     }
 }
 
