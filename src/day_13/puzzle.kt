f8eadc6962ce25179a51acca97a2729447705a7c
Dmitri Lakachauskis <d@dmitri.io>
2022-12-29 01:25:26 +0200

    Day 13 - puzzle 2.

--- src/day_13/puzzle.kt
+++ src/day_13/puzzle.kt
@@ -3,12 +3,12 @@ package day_13
 import println
 import readText
 
-sealed class Packet {
+sealed class Packet : Comparable<Packet> {
     companion object {
         fun fromLine(line: String) = if ('[' in line) Items.fromString(line) else Number.fromString(line)
     }
 
-    operator fun compareTo(other: Packet): Int = when (this) {
+    override operator fun compareTo(other: Packet): Int = when (this) {
         is Number -> when (other) {
             is Number -> compareTo(other)
             is Items -> asItems().compareTo(other)
@@ -78,10 +78,24 @@ fun puzzle1(input: String) = input
         (if (left < right) (index + 1) else 0) + acc
     }
 
+fun puzzle2(input: String): Int {
+    val dividers: Pair<Packet, Packet>
+
+    return "[[2]]\n[[6]]\n\n$input"
+        .lines()
+        .filter { it.isNotEmpty() }
+        .map { Packet.fromLine(it) }
+        .also { packets -> dividers = packets[0] to packets[1] }
+        .sorted()
+        .let { (it.indexOf(dividers.first) + 1) * (it.indexOf(dividers.second) + 1) }
+}
+
 fun main() {
     val testInput = readText("day_13/input_test")
     check(puzzle1(testInput) == 13)
+    check(puzzle2(testInput) == 140)
 
     val input = readText("day_13/input")
     puzzle1(input).println()
+    puzzle2(input).println()
 }
