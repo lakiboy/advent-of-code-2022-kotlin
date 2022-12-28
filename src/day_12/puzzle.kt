cb913a62cd9f9159fa4a2803808d1d0632fb75e6
Dmitri Lakachauskis <d@dmitri.io>
2022-12-28 15:22:52 +0200

    Day 12 - puzzle 2.

--- src/day_12/puzzle.kt
+++ src/day_12/puzzle.kt
@@ -15,6 +15,9 @@ data class Point(val x: Int, val y: Int) {
     }
 }
 
+private const val START = 1
+private const val FINISH = 26
+
 class HeightMap(lines: List<String>) {
     private lateinit var start: Point
     private lateinit var finish: Point
@@ -23,28 +26,47 @@ class HeightMap(lines: List<String>) {
             when (char) {
                 'S' -> {
                     start = Point(x, y)
-                    1
+                    START
                 }
                 'E' -> {
                     finish = Point(x, y)
-                    26
+                    FINISH
                 }
                 else -> char - 'a' + 1
             }
         }
     }
-    private val visited = mutableSetOf<Point>()
+
+    val distance get() = bfs(start)
+
+    val bestDistance
+        get() =
+            buildList {
+                grid.forEachIndexed { y, rows ->
+                    rows.indices.forEach { x ->
+                        Point(x, y)
+                            .takeIf { p -> p.value == START }
+                            ?.let { p -> bfs(p) }
+                            ?.let { distance -> add(distance) }
+                    }
+                }
+            }.min()
 
     private val Point.valid get() = x >= 0 && y >= 0 && y <= grid.lastIndex && x <= grid[y].lastIndex
 
-    private val Point.neighbours get() = Direction
-        .values()
-        .map { move(it) }
-        .filter { to -> to.valid && to !in visited && grid[y][x] + 1 >= grid[to.y][to.x] }
+    private val Point.value get() = grid[y][x]
 
-    val distance get(): Int {
+    private val Point.neighbours
+        get() = Direction
+            .values()
+            .map { move(it) }
+            .filter { to -> to.valid && value + 1 >= to.value }
+
+    private fun bfs(root: Point): Int? {
         val queue = mutableListOf<Pair<Point, Int>>()
-        queue.add(start to 0)
+        val visited = mutableSetOf<Point>()
+
+        queue.add(root to 0)
 
         while (queue.isNotEmpty()) {
             val (node, distance) = queue.removeFirst()
@@ -53,22 +75,26 @@ class HeightMap(lines: List<String>) {
                 return distance
             }
 
-            node.neighbours.forEach { neighbour ->
+            node.neighbours.filter { it !in visited }.forEach { neighbour ->
                 queue.add(neighbour to (distance + 1))
                 visited.add(neighbour)
             }
         }
 
-        return -1
+        return null
     }
 }
 
 fun puzzle1(lines: List<String>) = HeightMap(lines).distance
 
+fun puzzle2(lines: List<String>) = HeightMap(lines).bestDistance
+
 fun main() {
     val testInput = readInput("day_12/input_test")
     check(puzzle1(testInput) == 31)
+    check(puzzle2(testInput) == 29)
 
     val input = readInput("day_12/input")
     puzzle1(input).println()
+    puzzle2(input).println()
 }
