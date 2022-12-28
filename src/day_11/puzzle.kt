ffd54940669c1b35af4ae1ba3677f41e2c99bd0f
Dmitri Lakachauskis <d@dmitri.io>
2022-12-28 10:34:09 +0200

    Day 11 - puzzle 1 & 2.

--- src/day_11/puzzle.kt
+++ src/day_11/puzzle.kt
@@ -3,14 +3,10 @@ package day_11
 import println
 import readText
 
-typealias Predicate = (Long) -> Boolean
-
 typealias Expression = (Long) -> Long
 
-fun predicateFactory(divisor: Long) = { value: Long -> value % divisor == 0L }
-
 fun expressionFactory(expr: String) = { value: Long ->
-    val (a, op, b) = expr.replace("old", value.toString()).also { it.println() }.split(" ")
+    val (a, op, b) = expr.replace("old", value.toString()).split(" ")
     when (op) {
         "*" -> a.toLong() * b.toLong()
         "+" -> a.toLong() + b.toLong()
@@ -19,21 +15,19 @@ fun expressionFactory(expr: String) = { value: Long ->
     }
 }
 
-fun String.toPredicate() = predicateFactory(substringAfter(" by ").toLong())
-
-fun String.toExpression() = expressionFactory(substringAfter(" = "))
-
 data class Monkey(
     val id: Int,
     val items: MutableList<Long>,
     val trueId: Int,
     val falseId: Int,
     val expression: Expression,
-    val predicate: Predicate,
+    val divisor: Long,
 )
 
 class Game(private val monkeys: List<Monkey>, rounds: Int, relief: Long) {
     private val inspections = MutableList(monkeys.size) { 0 }
+
+    private val maxLevel = monkeys.fold(1L) { acc, monkey -> acc * monkey.divisor }
 
     val monkeyBusiness
         get() = inspections
@@ -45,12 +39,14 @@ class Game(private val monkeys: List<Monkey>, rounds: Int, relief: Long) {
     private fun Long.moveTo(monkeyId: Int) = monkeys[monkeyId].items.add(this)
 
     init {
+        val reduce: Expression = { value -> if (relief == 1L) value % maxLevel else value / relief }
+
         repeat(rounds) {
             monkeys.forEach { monkey ->
                 with(monkey) {
                     items.forEach { level ->
-                        val newLevel = expression(level) / relief
-                        val monkeyId = if (predicate(newLevel)) trueId else falseId
+                        val newLevel = reduce(expression(level))
+                        val monkeyId = if (newLevel % divisor == 0L) trueId else falseId
                         newLevel.moveTo(monkeyId)
                     }
                     inspections[id] += items.size
@@ -62,6 +58,10 @@ class Game(private val monkeys: List<Monkey>, rounds: Int, relief: Long) {
 }
 
 fun puzzle(input: String, rounds: Int, relief: Int): Long {
+    fun String.toExpression() = expressionFactory(substringAfter(" = "))
+
+    fun String.toDivisor() = substringAfter(" by ").toLong()
+
     val monkeys = input.split("\n\n").mapIndexed { index, commands ->
         val (itemsCmd, operationCmd, testCmd, trueOutcomeCmd, falseOutcomeCmd) = commands
             .split("\n")
@@ -79,7 +79,7 @@ fun puzzle(input: String, rounds: Int, relief: Int): Long {
             trueId = trueOutcomeCmd.substringAfter("monkey ").toInt(),
             falseId = falseOutcomeCmd.substringAfter("monkey ").toInt(),
             expression = operationCmd.toExpression(),
-            predicate = testCmd.toPredicate(),
+            divisor = testCmd.toDivisor()
         )
     }
 
@@ -88,10 +88,10 @@ fun puzzle(input: String, rounds: Int, relief: Int): Long {
 
 fun main() {
     val testInput = readText("day_11/input_test")
-//    check(puzzle(testInput, 20, 3) == 10_605L)
-    check(puzzle(testInput, 1, 1) == 2_713_310_158L)
+    check(puzzle(testInput, 20, 3) == 10_605L)
+    check(puzzle(testInput, 10_000, 1) == 2_713_310_158L)
 
-//    val input = readText("day_11/input")
-//    puzzle(input, 20, 3).println()
-//    puzzle(input, 10_000, 1).println()
+    val input = readText("day_11/input")
+    puzzle(input, 20, 3).println()
+    puzzle(input, 10_000, 1).println()
 }
