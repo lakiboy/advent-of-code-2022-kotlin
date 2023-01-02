f81a2664b86999febe091adc6e2ad3c785c55138
Dmitri Lakachauskis <d@dmitri.io>
2023-01-02 22:33:34 +0200

    Day 8 - puzzle 2 - better.

--- src/day_08/puzzle.kt
+++ src/day_08/puzzle.kt
@@ -24,11 +24,24 @@ class Grid private constructor(private val trees: List<List<Int>>) {
             (1 until (len - 1)).count { x -> Point(x, y).visible }
         }
 
-    private val Point.visible get() = Direction.values().any { visibleTowards(it.move, 1) }
+    val scenicScore
+        get() = (0 until len).maxOf { y ->
+            (0 until len).maxOf { x -> Point(x, y).visibleTrees }
+        }
+
+    private val Point.visible get() = Direction.values().any { direction ->
+        visibleTowards(direction.move, 1)
+    }
+
+    private val Point.visibleTrees get() = Direction.values().fold(1) { acc, direction ->
+        acc * visibleCount(direction.move, 1)
+    }
 
     private val Point.value get() = trees[y][x]
 
     private val Point.edge get() = x == 0 || y == 0 || x == len - 1 || y == len - 1
+
+    private val Point.invalid get() = x !in 0 until len || y !in 0 until len
 
     private tailrec fun Point.visibleTowards(move: Point, distance: Int): Boolean {
         val neighbour = move * distance + this
@@ -37,6 +50,16 @@ class Grid private constructor(private val trees: List<List<Int>>) {
             neighbour.value >= value -> false
             neighbour.edge -> true
             else -> visibleTowards(move, distance + 1)
+        }
+    }
+
+    private tailrec fun Point.visibleCount(move: Point, distance: Int): Int {
+        val neighbour = move * distance + this
+
+        return when {
+            neighbour.invalid -> distance - 1
+            neighbour.value >= value -> distance
+            else -> visibleCount(move, distance + 1)
         }
     }
 
@@ -53,10 +76,14 @@ class Grid private constructor(private val trees: List<List<Int>>) {
 
 fun puzzle1(lines: List<String>) = Grid.of(lines).visibleCount
 
+fun puzzle2(lines: List<String>) = Grid.of(lines).scenicScore
+
 fun main() {
     val testInput = readInput("day_08/input_test")
     check(puzzle1(testInput) == 21)
+    check(puzzle2(testInput) == 8)
 
     val input = readInput("day_08/input")
     puzzle1(input).println()
+    puzzle2(input).println()
 }
