75fc543962b02765ea7a500f94c3eeababd362d2
Dmitri Lakachauskis <d@dmitri.io>
2023-01-12 01:03:27 +0200

    Demo benchmark.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
+import kotlinx.benchmark.gradle.JvmBenchmarkTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.22"
-    id("org.jetbrains.kotlinx.benchmark") version "0.4.6"
+    kotlin("plugin.allopen") version "1.7.22"
+    id("org.jetbrains.kotlinx.benchmark") version "0.4.7"
     id("io.gitlab.arturbosch.detekt") version "1.22.0"
 }
 
@@ -11,6 +13,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-benchmark-runtime:0.4.7")
     testImplementation(kotlin("test"))
     detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.22.0")
 }
@@ -26,7 +29,26 @@ tasks {
     }
 }
 
+allOpen {
+    annotation("org.openjdk.jmh.annotations.State")
+}
+
 detekt {
     config = files("detekt-config.yml")
     buildUponDefaultConfig = true
+}
+
+benchmark {
+    configurations {
+        named("main") {
+            iterationTime = 5
+            iterationTimeUnit = "sec"
+        }
+    }
+    targets {
+        register("main") {
+            this as JvmBenchmarkTarget
+            jmhVersion = "1.36"
+        }
+    }
 }
