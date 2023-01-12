23b8581bd17fb74f056abb648efb7df2e36d3154
Dmitri Lakachauskis <d@dmitri.io>
2023-01-12 15:02:57 +0200

    Move benchmarks to separate dir.

--- src/benchmarks/kotlin/io/dmitrijs/aoc2022/Day10Benchmark.kt
+++ src/benchmarks/kotlin/io/dmitrijs/aoc2022/Day10Benchmark.kt
@@ -2,18 +2,12 @@ package io.dmitrijs.aoc2022
 
 import io.dmitrijs.aoc2022.Resources.resourceAsLines
 import kotlinx.benchmark.Benchmark
-import kotlinx.benchmark.Measurement
 import kotlinx.benchmark.Scope
 import kotlinx.benchmark.Setup
 import kotlinx.benchmark.State
-import kotlinx.benchmark.Warmup
-import org.openjdk.jmh.annotations.Fork
 
 @State(Scope.Benchmark)
-@Warmup(iterations = 1)
-@Measurement(iterations = 4)
-@Fork(1)
-class Day10Benchmark {
+internal class Day10Benchmark {
     private lateinit var input: List<String>
 
     @Setup
