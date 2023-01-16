15ff7c41ef5a71736a5a9dc1f77601d101c177cb
Dmitri Lakachauskis <d@dmitri.io>
2023-01-16 21:36:16 +0200

    Unify day 12. (#3)

---
+++ src/benchmarks/kotlin/io/dmitrijs/aoc2022/Day12Benchmark.kt
@@ -0,0 +1,23 @@
+package io.dmitrijs.aoc2022
+
+import io.dmitrijs.aoc2022.Resources.resourceAsLines
+import kotlinx.benchmark.Benchmark
+import kotlinx.benchmark.Scope
+import kotlinx.benchmark.Setup
+import kotlinx.benchmark.State
+
+@State(Scope.Benchmark)
+internal class Day12Benchmark {
+    private lateinit var input: List<String>
+
+    @Setup
+    fun setUp() {
+        input = resourceAsLines("day12")
+    }
+
+    @Benchmark
+    fun puzzle1Bfs() = Day12(input).puzzle1()
+
+    @Benchmark
+    fun puzzle1Dijkstra() = Day12(input).puzzle1Dijkstra()
+}
--- src/main/kotlin/io/dmitrijs/aoc2022/Day12.kt
+++ src/main/kotlin/io/dmitrijs/aoc2022/Day12.kt
@@ -1,5 +1,7 @@
 package io.dmitrijs.aoc2022
 
+import java.util.PriorityQueue
+
 class Day12(input: List<String>) {
     private lateinit var start: Point
     private lateinit var finish: Point
@@ -19,50 +21,99 @@ class Day12(input: List<String>) {
         }
     }
 
-    fun puzzle1() = bfs(start)
+    fun puzzle1() = bfs(start, incDec = 1) { it == finish }
 
-    fun puzzle2() =
-        buildList {
-            grid.forEachIndexed { y, rows ->
-                rows.indices.forEach { x ->
-                    Point(x, y)
-                        .takeIf { p -> p.value == START }
-                        ?.let { p -> bfs(p) }
-                        ?.let { distance -> add(distance) }
-                }
-            }
-        }.min()
+    fun puzzle2() = bfs(finish, incDec = -1) { it.value == START }
+
+    fun puzzle1Dijkstra() = dijkstra(start)
 
     private val Point.valid get() = x >= 0 && y >= 0 && y <= grid.lastIndex && x <= grid[y].lastIndex
 
     private val Point.value get() = grid[y][x]
 
-    private val Point.neighbours
-        get() = Direction
+    private fun Point.neighbours(incDec: Int): List<Point> {
+        return Direction
             .values()
             .map { this + it }
-            .filter { to -> to.valid && value + 1 >= to.value }
+            .filter { to -> to.valid }
+            .filter { to -> if (incDec > 0) value + incDec >= to.value else value + incDec <= to.value }
+    }
 
-    private fun bfs(root: Point): Int? {
+    private fun bfs(root: Point, incDec: Int, done: (Point) -> Boolean): Int {
         val queue = ArrayDeque<Pair<Point, Int>>()
-        val visited = mutableSetOf<Point>()
+        val visited = hashSetOf<Point>()
 
         queue.add(root to 0)
 
         while (queue.isNotEmpty()) {
             val (node, distance) = queue.removeFirst()
 
-            if (node == finish) {
+            if (done(node)) {
                 return distance
             }
 
-            node.neighbours.filter { it !in visited }.forEach { neighbour ->
+            node.neighbours(incDec).filter { it !in visited }.forEach { neighbour ->
                 queue.add(neighbour to (distance + 1))
                 visited.add(neighbour)
             }
         }
 
-        return null
+        return -1
+    }
+
+    private fun dijkstra(root: Point): Int {
+        val queue = PriorityQueue<Visit>()
+        val cost = hashMapOf<Point, Int>()
+
+        queue.add(Visit(root, 0))
+        cost[start] = 0
+
+        while (queue.isNotEmpty()) {
+            val (node, distance) = queue.poll()
+
+            if (node == finish) {
+                return distance
+            }
+
+            val newCost = cost.getValue(node) + 1
+
+            node.neighbours(incDec = 1).forEach { neighbour ->
+                if (neighbour !in cost || newCost < cost.getValue(neighbour)) {
+                    cost[neighbour] = newCost
+                    queue.add(Visit(neighbour, newCost))
+                }
+            }
+        }
+
+        return -1
+    }
+
+    /*
+    private fun greedyBfs(root: Point): Int {
+        val queue = PriorityQueue<Visit>()
+        val visited = mutableSetOf<Point>()
+
+        queue.add(Visit(root, 0, root.distanceTo(finish)))
+
+        while (queue.isNotEmpty()) {
+            val (node, distance) = queue.poll()
+
+            if (node == finish) {
+                return distance
+            }
+
+            node.neighbours(incDec = 1).filter { it !in visited }.forEach { neighbour ->
+                queue.add(Visit(neighbour, distance + 1, neighbour.distanceTo(finish)))
+                visited.add(neighbour)
+            }
+        }
+
+        return -1
+    }
+    */
+
+    private data class Visit(val p: Point, val d: Int) : Comparable<Visit> {
+        override fun compareTo(other: Visit) = d.compareTo(other.d)
     }
 
     companion object {
