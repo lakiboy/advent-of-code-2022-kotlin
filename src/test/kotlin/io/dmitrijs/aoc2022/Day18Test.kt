aa3961c95b1871ef9c6e22f23e56558059eaa25c
Dmitri Lakachauskis <d@dmitri.io>
2023-01-29 16:18:38 +0200

    Day 18 - Puzzle 1.

---
+++ src/test/kotlin/io/dmitrijs/aoc2022/Day18Test.kt
@@ -0,0 +1,27 @@
+package io.dmitrijs.aoc2022
+
+import io.dmitrijs.aoc2022.Resources.resourceAsLines
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+@DisplayName("Day 18")
+internal class Day18Test {
+    private val exampleInput = resourceAsLines("day18_example")
+    private val problemInput = resourceAsLines("day18")
+
+    @Nested
+    @DisplayName("Puzzle 1")
+    inner class Puzzle1 {
+        @Test
+        fun `solves example`() {
+            assertEquals(64, Day18(exampleInput).puzzle1())
+        }
+
+        @Test
+        fun `solves problem`() {
+            assertEquals(4_364, Day18(problemInput).puzzle1())
+        }
+    }
+}
