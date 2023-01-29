95cc95348662a9eb0d87fe3db88bf11d3d165a0b
Dmitri Lakachauskis <d@dmitri.io>
2023-01-29 22:36:58 +0200

    Day 18 - Puzzle 2 - WIP.

--- src/main/kotlin/io/dmitrijs/aoc2022/Day18.kt
+++ src/main/kotlin/io/dmitrijs/aoc2022/Day18.kt
@@ -3,18 +3,44 @@ package io.dmitrijs.aoc2022
 import kotlin.math.absoluteValue
 
 class Day18(input: List<String>) {
-    private val points = input.map { line ->
-        val (x, y, z) = line.split(",").map(String::toInt)
-        Point(x, y, z)
+    private val points =
+        input.map { line ->
+            val (x, y, z) = line.split(",").map(String::toInt)
+            Point(x, y, z)
+        }.toSet()
+
+    fun puzzle1() = surfaceSize(points)
+
+    fun puzzle2() = puzzle1() - surfaceSize(points.innerList())
+
+    private fun Set<Point>.innerList(): Set<Point> {
+        val minX = minOf { it.x }
+        val minY = minOf { it.y }
+        val minZ = minOf { it.z }
+        val maxX = maxOf { it.x }
+        val maxY = maxOf { it.y }
+        val maxZ = maxOf { it.z }
+
+        for (x in minX..maxX) {
+            for (y in minY..maxY) {
+                for (z in minZ..maxZ) {
+                    Point(x, y, z).takeUnless { it in this }?.also { println(it) }
+                }
+            }
+        }
+
+        return setOf(
+            Point(x = 2, y = 2, z = 5)
+        )
     }
 
-    fun puzzle1() = points
+    private fun surfaceSize(items: Set<Point>) = items
         .map { point ->
-            points.count { adjacent -> adjacent.connectedTo(point) }
+            items.count { adjacent -> adjacent.connectedTo(point) }
         }
         .sumOf { commonSides -> CUBE_SIDES - commonSides }
 
-    private data class Point(private val x: Int, private val y: Int, private val z: Int) {
+    private data class Point(val x: Int, val y: Int, val z: Int) : Comparable<Point> {
         fun connectedTo(other: Point) =
             sides(other).count { (a, b) -> a == b } == 2 && sides(other).any { (a, b) -> (a - b).absoluteValue == 1 }
 
@@ -23,6 +49,9 @@ class Day18(input: List<String>) {
             y to other.y,
             z to other.z,
         )
+
+        override fun compareTo(other: Point) =
+            sides(other).firstOrNull { (a, b) -> a != b }?.let { (a, b) -> a - b } ?: 0
     }
 
     companion object {
